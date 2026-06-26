077aae3b084d0eb8b78c5a2511aa21f9c7905160
Barbora Hornakova <xhornak2@fi.muni.cz>
2026-06-27 01:57:50 +0200

    feat(controller/court): add court controller

---
+++ src/main/java/cz/hornakova/barbora/tennisclub/controller/CourtController.java
@@ -0,0 +1,50 @@
+package cz.hornakova.barbora.tennisclub.controller;
+
+import cz.hornakova.barbora.tennisclub.model.dto.CourtCreateRequest;
+import cz.hornakova.barbora.tennisclub.model.dto.CourtResponse;
+import cz.hornakova.barbora.tennisclub.model.dto.CourtUpdateRequest;
+import cz.hornakova.barbora.tennisclub.service.CourtService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courts")
+public class CourtController {
+    
+    private final CourtService courtService;
+
+    public CourtController(CourtService courtService) {
+        this.courtService = courtService;
+    }
+
+    @GetMapping
+    public List<CourtResponse> getAll() {
+        return courtService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public CourtResponse getById(@PathVariable Long id) {
+        return courtService.getById(id);
+    }
+
+    @PostMapping
+    public CourtResponse create(
+            @RequestBody CourtCreateRequest request
+    ) {
+        return courtService.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public CourtResponse update(
+            @PathVariable Long id,
+            @RequestBody CourtUpdateRequest request
+    ) {
+        return courtService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        courtService.delete(id);
+    }
+}
