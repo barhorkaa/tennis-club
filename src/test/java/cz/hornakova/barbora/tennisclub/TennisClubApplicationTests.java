7e897af3e35ffa1a1f4f2b5399ba92bcd1d73179
Barbora Hornakova <xhornak2@fi.muni.cz>
2026-06-26 15:00:41 +0200

    feat(project): project setup

---
+++ src/test/java/cz/hornakova/barbora/tennisclub/TennisClubApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.hornakova.barbora.tennisclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
