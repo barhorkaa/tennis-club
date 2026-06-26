a97215b4c264d1052516851749db0f34e492c744
Barbora Hornakova <xhornak2@fi.muni.cz>
2026-06-27 01:49:03 +0200

    feat(dao/surfaceType): add surface type dao

---
+++ src/main/java/cz/hornakova/barbora/tennisclub/dao/SurfaceTypeDao.java
@@ -0,0 +1,6 @@
+package cz.hornakova.barbora.tennisclub.dao;
+
+import cz.hornakova.barbora.tennisclub.model.entity.SurfaceType;
+
+public interface SurfaceTypeDao extends Dao<SurfaceType> {
+}
---
+++ src/main/java/cz/hornakova/barbora/tennisclub/dao/impl/SurfaceTypeDaoImpl.java
@@ -0,0 +1,62 @@
+package cz.hornakova.barbora.tennisclub.dao.impl;
+
+import cz.hornakova.barbora.tennisclub.dao.SurfaceTypeDao;
+import cz.hornakova.barbora.tennisclub.model.entity.SurfaceType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public class SurfaceTypeDaoImpl implements SurfaceTypeDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public List<SurfaceType> getAll() {
+        return em.createQuery(
+                        """
+                           SELECT st
+                           FROM SurfaceType st
+                           WHERE st.deleted = false
+                           """,
+                        SurfaceType.class)
+                .getResultList();
+    }
+
+    @Override
+    public Optional<SurfaceType> getById(Long id) {
+        SurfaceType surfaceType = em.find(SurfaceType.class, id);
+
+        if (surfaceType == null || surfaceType.isDeleted()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(surfaceType);
+    }
+
+    @Override
+    public SurfaceType save(SurfaceType surfaceType) {
+//        if (surfaceType.getId() == null) {
+            em.persist(surfaceType);
+            return surfaceType;
+//        }
+    }
+
+    @Override
+    public void update(SurfaceType surfaceType) {
+        em.merge(surfaceType);
+    }
+
+    @Override
+    public void delete(SurfaceType surfaceType) {
+        surfaceType.setDeleted(true);
+
+        em.merge(surfaceType);
+    }
+}
