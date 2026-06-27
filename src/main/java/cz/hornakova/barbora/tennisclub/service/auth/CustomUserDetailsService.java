c059c574993b924ff848d444e31823c7fcecf3c7
Barbora Hornakova <xhornak2@fi.muni.cz>
2026-06-27 17:06:37 +0200

    feat(service/auth/user): add user details service for auth

---
+++ src/main/java/cz/hornakova/barbora/tennisclub/service/auth/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package cz.hornakova.barbora.tennisclub.service.auth;
+
+import cz.hornakova.barbora.tennisclub.dao.UserDao;
+import cz.hornakova.barbora.tennisclub.model.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserDao userDao;
+
+    public CustomUserDetailsService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+
+        User user = userDao.getByUsername(username)
+                .orElseThrow();
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
