f3bd6e0d54957c7f259ea2343ba7603a4ca85ac1
Barbora Hornakova <xhornak2@fi.muni.cz>
2026-06-27 17:07:50 +0200

    feat(service/jwt): add jwt service

---
+++ src/main/java/cz/hornakova/barbora/tennisclub/service/auth/JwtService.java
@@ -0,0 +1,39 @@
+package cz.hornakova.barbora.tennisclub.service.auth;
+
+import cz.hornakova.barbora.tennisclub.model.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.access-expiration}")
+    private long expiration;
+
+    public String generateToken(User user) {
+
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .claim("role", user.getRole().name())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
